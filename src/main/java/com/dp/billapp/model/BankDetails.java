d2ba626d71996f5c8202a6d569726d9e5d357265
SHREYA RAKSHIT <shreyarakshit04@gmail.com>
2023-02-06 20:32:08 +0530

    bank details and showroom model added

---
+++ src/main/java/com/dp/billapp/model/BankDetails.java
@@ -0,0 +1,28 @@
+package com.dp.billapp.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+@Builder
+@Entity
+@Table(name = "bank_details")
+public class BankDetails {
+
+    @Id
+    @GeneratedValue(strategy= GenerationType.IDENTITY)
+    @Column(name = "id")
+    private long id;
+
+    @Column(name = "account_number")
+    private String accountNumber;
+
+    @Column(name = "IFSC_Code")
+    private String ifscCode;
+}
