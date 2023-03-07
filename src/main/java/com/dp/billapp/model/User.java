0409df22ac5c2ec356bff27643558aa08a9b0d1d
SHREYA RAKSHIT <shreyarakshit04@gmail.com>
2023-03-07 16:10:27 +0530

    employee additional added

--- src/main/java/com/dp/billapp/model/User.java
+++ src/main/java/com/dp/billapp/model/User.java
@@ -1,11 +1,19 @@
 package com.dp.billapp.model;
 
 
+import com.vladmihalcea.hibernate.type.array.IntArrayType;
+import com.vladmihalcea.hibernate.type.array.StringArrayType;
+import com.vladmihalcea.hibernate.type.json.JsonBinaryType;
+import com.vladmihalcea.hibernate.type.json.JsonNodeBinaryType;
+import com.vladmihalcea.hibernate.type.json.JsonNodeStringType;
+import com.vladmihalcea.hibernate.type.json.JsonStringType;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import org.hibernate.annotations.Type;
+import org.hibernate.annotations.TypeDef;
+import org.hibernate.annotations.TypeDefs;
 import org.springframework.security.core.SpringSecurityCoreVersion;
 
 import javax.persistence.*;
@@ -16,6 +24,14 @@ import javax.persistence.*;
 @Builder
 @Entity
 @Table(name = "users",schema = "public")
+@TypeDefs({
+        @TypeDef(name = "string-array", typeClass = StringArrayType.class),
+        @TypeDef(name = "int-array", typeClass = IntArrayType.class),
+        @TypeDef(name = "json", typeClass = JsonStringType.class),
+        @TypeDef(name = "jsonb", typeClass = JsonBinaryType.class),
+        @TypeDef(name = "jsonb-node", typeClass = JsonNodeBinaryType.class),
+        @TypeDef(name = "json-node", typeClass = JsonNodeStringType.class),
+})
 public class User {
 
     private static final long serialVersionUID = SpringSecurityCoreVersion.SERIAL_VERSION_UID;
@@ -42,4 +58,8 @@ public class User {
 
     @Column(name = "address")
     private String address;
+    @Type(type = "json")
+    @Column(name = "additional_info", columnDefinition = "json")
+    private AdditionalUserDetails additionalUserDetails;
+
 }
